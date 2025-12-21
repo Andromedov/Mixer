fec37bfa01ca5bce1420af38dff32e48ae659b28
Andromedov <andromedov.mod@gmail.com>
2025-12-21 13:48:05 +0200

    [chore] bump version to 2.1.2

--- build.gradle.kts
+++ build.gradle.kts
@@ -12,7 +12,7 @@ allprojects {
     apply(plugin = "maven-publish")
 
     group = "net.somewhatcity"
-    version = "2.1.1"
+    version = "2.1.2"
 
     repositories {
         maven("https://repo.papermc.io/repository/maven-public/")
